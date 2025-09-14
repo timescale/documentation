1573d7931cdd1374b65c7c5300d4fda17c7b1268
robsoncoelho <robson.amc@gmail.com>
2025-09-14 12:54:06 +0100

    chore: homepage responsivee

--- snippets/card.jsx
+++ snippets/card.jsx
@@ -2,7 +2,7 @@
 
 export const CardComponent = ({children, icon, title, href, cta}) => {
   return (
-    <a href={href} className="group/item flex gap-3 flex-col pt-6 px-6 pb-9 rounded-2xl bg-white dark:bg-background-dark border border-gray-950/10 dark:border-white/10 overflow-hidden w-full cursor-pointer hover:!border-primary dark:hover:!border-primary-light">
+    <a href={href} className="group/item flex gap-3 flex-col my-2 pt-6 px-6 pb-9 rounded-2xl bg-white dark:bg-background-dark border border-gray-950/10 dark:border-white/10 overflow-hidden w-full cursor-pointer hover:!border-primary dark:hover:!border-primary-light">
       <div className="bg-[#F8F9FF] inline-flex items-center justify-center rounded-lg self-start w-[50px] h-[50px]">
         <Icon icon={icon} size={24} color="#6447FB" />
       </div>
